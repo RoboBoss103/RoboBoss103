5fa0a1d7fcd0cdd85b733d14f977b03d90c043a9
RoboBoss103 <gg1100@hpsk12.net>
2021-10-04 09:22:20 -0400

    r

---
+++ new 1.java
@@ -0,0 +1,53 @@
+x=7
+
+y=3
+
+Z=6
+
+if(x>2 && y<2 || z==5){
+
+	print(statement 1);
+
+	if(x>3 || z<5){
+
+		print(statement 5);
+
+	}
+
+}
+
+else if (x<2 && y<2){
+
+	print (statement 2);
+
+	if(z>5){
+
+		print(statement 6);
+
+	}
+
+	else{
+
+		print(statement 7);
+
+	}
+
+}
+
+else if (x>2 && y>2){
+
+	print (statement 3);
+
+}
+
+else{
+
+	if(x>3 || z<5){
+
+		print(statement 8;
+
+	}
+
+	print(statement 4);
+
+}
---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+        int x=0;
+        while(x<4){
+            int y=0;
+            while(y<5){
+                System.out.println("Hello World");
+                y = y+1;
+            }
+            x=x+1;
+        }
+    }
+}
